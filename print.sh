6e762a6b668a773ba92ea0dd46345c423a5d091b
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-05-27 18:33:38 +0530

    listing down the topic

--- print.sh
+++ print.sh
@@ -1,1 +1,9 @@
 echo Hello world
+#Print the message in color
+#syntax : echo -e "/e[COLmMessage\e[0m"
+echo -e "/e[31mHello in RED Color\e[0m"
+echo -e "/e[32mHello in GREEN Color\e[0m"
+echo -e "/e[33mHello in YELLOW color\e[0m"
+echo -e "/e[34mHello in BLUE in color\e[0m"
+echo -e "/e[35mHellow in MEGENTA color\e[0m"
+echo -e "/e[36mHellow in CIYAN color\e[0m"
